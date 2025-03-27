796ffaf4a5ab235d38652ebcf0c4dff370c52831
Taylor King <taylorking206@u.boisestate.edu>
2025-03-27 12:42:14 -0600

    init landing pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { Struct } from 'drizzle-struct/front-end';
 	import { browser } from '$app/environment';
+	import MainNav from '$lib/components/general/MainNav.svelte';
+	import Footer from '$lib/components/general/Footer.svelte';
 
 	setTimeout(() => {
 		if (browser) Struct.buildAll();
@@ -9,5 +11,7 @@
 </script>
 
 <main>
+	<MainNav />
 	{@render children()}
+	<Footer />
 </main>
--- src/routes/our-plans/+page.svelte
+++ src/routes/our-plans/+page.svelte
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row mb-3">
         <div class="col">
-            <h1 class="text-center">Contact</h1>
+            <h1 class="text-center">Our Plans</h1>
         </div>
     </div>
 </div>
