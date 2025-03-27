dab7a6843a5d3f355a5fc12aa1e1887dd9bd67e1
Taylor King <taylorking206@u.boisestate.edu>
2025-03-27 08:44:12 -0600

    mailer listeners

--- src/lib/server/structs/mailer.ts
+++ src/lib/server/structs/mailer.ts
@@ -4,6 +4,7 @@ import { Email } from "./email";
 import { PUBLIC_APP_NAME, PUBLIC_DOMAIN } from "$env/static/public";
 import { PORT } from "$env/static/private";
 import { HTTPS } from "$env/static/private";
+import terminal from "../utils/terminal";
 
 export namespace Mailer {
     export const MailingList = new Struct({
@@ -16,16 +17,46 @@ export namespace Mailer {
     });
 
     MailingList.on('create', async (mailer) => {
-        Email.send({
+        const link = `${HTTPS === 'true' ? 'https' : 'http'}://${PUBLIC_DOMAIN === 'localhost' ? PUBLIC_DOMAIN + ':' + PORT : PUBLIC_DOMAIN.toLowerCase()}.com`;
+        const home = await Email.createLink(link);
+        const unsub = await Email.createLink(`${link}/unsubscribe/${mailer.data.email}`);
+        if (home.isErr()) {
+            return terminal.error(home.error);
+        }
+        if (unsub.isErr()) {
+            return terminal.error(unsub.error);
+        }
+        const res = await Email.send({
             type: 'welcome',
             data: {
-                url: `${HTTPS === 'true' ? 'https' : 'http'}://${PUBLIC_DOMAIN === 'localhost' ? PUBLIC_DOMAIN + ':' + PORT : PUBLIC_DOMAIN.toLowerCase()}.com`,
+                url: home.value,
                 name: mailer.data.name,
+                unsubscribe: unsub.value,
             },
             to: mailer.data.email,
             subject: `Welcome to ${PUBLIC_APP_NAME}!`,
-        })
+        });
+        if (res.isErr()) {
+            terminal.error(res.error);
+        }
     });
 
-    MailingList.on('delete', (data) => {});
+    MailingList.on('delete', async (mailer) => {
+        const link = `${HTTPS === 'true' ? 'https' : 'http'}://${PUBLIC_DOMAIN === 'localhost' ? PUBLIC_DOMAIN + ':' + PORT : PUBLIC_DOMAIN.toLowerCase()}.com`;
+        const resub = await Email.createLink(`${link}/join/${mailer.data.email}`);
+        if (resub.isErr()) {
+            return terminal.error(resub.error);
+        }
+        const res = await Email.send({
+            type: 'goodbye',
+            data: {
+                resubscribe: resub.value,
+            },
+            to: mailer.data.email,
+            subject: `Goodbye from ${PUBLIC_APP_NAME}!`,
+        });
+        if (res.isErr()) {
+            terminal.error(res.error);
+        }
+    });
 }
--- src/lib/types/email.ts
+++ src/lib/types/email.ts
@@ -14,6 +14,7 @@ export type Email = {
 'welcome': {
         name: string;
         url: string;
+        unsubscribe: string;
     };
 
 };
